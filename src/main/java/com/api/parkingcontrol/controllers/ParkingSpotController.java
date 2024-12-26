2e289bcc382b5608dac029ede8bb4654b5e12731
Lucau Rafael <72351063+luradev@users.noreply.github.com>
2024-12-26 15:51:19 +0100

    First commit

---
+++ src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
@@ -0,0 +1,16 @@
+package com.api.parkingcontrol.controllers;
+
+import com.api.parkingcontrol.services.ParkingSpotService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+public class ParkingSpotController {
+
+    final ParkingSpotService parkingSpotService;
+
+    public ParkingSpotController(ParkingSpotService parkingSpotService){
+        this.parkingSpotService = parkingSpotService;
+    }
+}
